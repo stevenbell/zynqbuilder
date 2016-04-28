f52f1b22d30941567c78693fdc68b7209ede45d1
Jing Pu <jimmypu07@gmail.com>
2016-04-28 11:03:44 -0700

    added a procedure to pull FRMPTR_STS (0x24) register in vdma for current frame buffer location, but it doesn't work due to vdma internal errors.

--- drivers/common.h
+++ drivers/common.h
@@ -3,7 +3,7 @@
 
 #define ERROR(...)   printk(__VA_ARGS__)
 #define WARNING(...) if(debug_level > 0) printk(__VA_ARGS__)
-#define DEBUG(...)   if(debug_level > 1) printk(__VA_ARGS__)
-#define TRACE(...)   if(debug_level > 2) printk(__VA_ARGS__)
+#define TRACE(...)   if(debug_level > 1) printk(__VA_ARGS__)
+#define DEBUG(...)   if(debug_level > 2) printk(__VA_ARGS__)
 
 #endif
